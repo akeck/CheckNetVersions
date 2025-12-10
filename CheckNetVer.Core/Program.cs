b52e0639242fc4a6f9ce6d03168eb760273741d1
Keck Alexander <alexander.keck@acp.at>
2025-12-10 22:42:03 +0100

    Add project files.

---
+++ CheckNetVer.Core/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CheckNetVer.Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new();
+            string dir = Directory.GetCurrentDirectory();
+            List<string> files = Directory.GetFiles(dir, "*.dll", SearchOption.AllDirectories).ToList();
+            files.AddRange(Directory.GetFiles(dir, "*.exe", SearchOption.AllDirectories).ToList());
+            foreach (string file in files)
+            {
+                string ver = GetTargetFramework(file);
+                if(!string.IsNullOrWhiteSpace(ver))
+                    sb.AppendLine($"{file};{ver}");
+
+            }
+            File.WriteAllText(Path.Combine(dir,"FrameworkReport.csv"), sb.ToString());
+        }
+
+        private static readonly Regex CompiledNetCoreRegex = new Regex(@".NETCoreApp,Version=v[0-9\.]+", RegexOptions.Compiled);
+        private static readonly Regex CompiledNetFrameworkRegex = new Regex(@".NETFramework,Version=v[0-9\.]+", RegexOptions.Compiled);
+
+        // You can define other methods, fields, classes and namespaces here
+        public static string GetTargetFramework(string file)
+        {
+            string contents = File.ReadAllText(file);
+
+            Match match = CompiledNetCoreRegex.Match(contents);
+            if (match.Success)
+                return match.Value;
+
+            match = CompiledNetFrameworkRegex.Match(contents);
+            return match.Success ? match.Value : null;
+        }
+    }
+}
---
+++ CheckNetVer.Core/ReadMe.md
@@ -0,0 +1,5 @@
+﻿# CheckNetVersions
+
+Scans the current folder and all subfolders for .dll and .exe files. For each file found that uses the .NET Framework (not .NET Core or .NET 5.0 and above), it generates a line containing the full path and the required version. The collected entries are saved into the file *FrameworkReport.csv*.
+
+I added a deployment profile to publish a self-contained executable for Windows x64.
